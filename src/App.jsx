9d5c2fdcffb7fb1431f68cac780cd54f42c3cf9b
devcarolinealmeida <carolineitalia85@gmail.com>
2025-06-30 13:32:21 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <input></input>
+      <button>Add</button>
+      <div className="output">
+        <p>Output will be displayed here</p>
+      </div>
+    </>
+  )
+}
+
+export default App
